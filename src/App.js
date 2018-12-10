aa897558db0d00e0de17ff0332f1bece21dc289c
Harry Nicholls <harry.m.nicholls@gmail.com>
2018-12-09 19:29:04 -0500

    Adds readability and axios to react app.
    Removes initial react app jsx and styles.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+import Readability from 'readability';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textContent: '',
+      parsedArticle: {}
+    };
+  }
+
+  componentDidMount() {
+    this.getPage();
+  }
+
+  getPage = () => {
+    axios.get('http://localhost:8000/').then(
+      res => {
+        console.log(res);
+        const doc = document.implementation.createHTMLDocument('New Document');
+        const frag = document.createRange().createContextualFragment(res.data);
+        doc.body.appendChild(frag);
+        console.log('frag', frag);
+        console.log('doc', doc);
+
+        const article = new Readability(doc);
+        console.log('article', article);
+        const parsedArticle = article.parse();
+        console.log('parsed', parsedArticle);
+        this.setState({
+          textContent: parsedArticle.textContent || '',
+          parsedArticle
+        });
+      },
+      err => {
+        console.error(err);
+      }
+    );
+  };
+
   render() {
+    const { textContent, parsedArticle } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {parsedArticle.content}
+        <p className="text-content">{textContent}</p>
       </div>
     );
   }
