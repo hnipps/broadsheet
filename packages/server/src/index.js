6c0f1a4fb3ab715b0b2ca9238200a8e10bd6b842
Harry Nicholls <harry.m.nicholls@gmail.com>
2018-12-09 19:29:18 -0500

    Adds server for requesting web page

---
+++ packages/server/src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+const rp = require('request-promise');
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+app.get('/', (req, res) => {
+  rp(
+    'https://blog.prototypr.io/simplify-styling-with-functional-css-7b3e4edc2243'
+  )
+    .then(html => {
+      //success!
+      console.log(html);
+      res.send(html);
+    })
+    .catch(err => {
+      console.error(err);
+      res.send(err);
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
